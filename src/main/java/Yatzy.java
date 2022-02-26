efa3ce929d041e995f3673d67be6694e46575458
Guillaume LEGALL <guillaume.legall.ext@orange.com>
2022-02-26 15:27:36 +0100

    refacto on fullHouse

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -61,6 +61,13 @@ public class Yatzy {
         return occurences.isEmpty() ? 0 : occurences.keySet().stream().reduce(0, (prev, current) -> prev + (current * 4));
     }
 
+    public static int fullHouse(Roll roll) {
+        Map<Integer, Integer> occurences = getMinimumOccurencesMap(roll, 2);
+        return (occurences.size() < 2 ||
+                occurences.values().stream().mapToInt(v -> v).max().orElse(0) < 3) ?
+                0 : roll.dices.stream().reduce(0, Integer::sum);
+    }
+
     public static Map<Integer, Integer> getMinimumOccurencesMap(Roll roll, int occurence) {
         Map<Integer, Integer> result = roll.dices.stream()
                 .collect(Collectors.toMap(Function.identity(), value -> 1, Integer::sum));
@@ -78,60 +85,6 @@ public class Yatzy {
         return (roll.dices.stream().distinct().count() < 5 ||
                 roll.dices.stream().mapToInt(v -> v).min().orElse(6) < 2)
                 ? 0 : 20;
-    }
-    public static int largeStraight(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-        if (tallies[1] == 1 &&
-            tallies[2] == 1 &&
-            tallies[3] == 1 &&
-            tallies[4] == 1
-            && tallies[5] == 1)
-            return 20;
-        return 0;
-    }
-
-    public static int fullHouse(int d1, int d2, int d3, int d4, int d5)
-    {
-        int[] tallies;
-        boolean _2 = false;
-        int i;
-        int _2_at = 0;
-        boolean _3 = false;
-        int _3_at = 0;
-
-
-
-
-        tallies = new int[6];
-        tallies[d1-1] += 1;
-        tallies[d2-1] += 1;
-        tallies[d3-1] += 1;
-        tallies[d4-1] += 1;
-        tallies[d5-1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i+1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i+1;
-            }
-
-        if (_2 && _3)
-            return _2_at * 2 + _3_at * 3;
-        else
-            return 0;
     }
 }
 
